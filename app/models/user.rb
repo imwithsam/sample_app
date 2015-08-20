6101a7ac1207b225ac7112e58a52143e572ddc8f
Samson Brock <SamsonLB@gmail.com>
2015-08-20 15:00:24 -0600

    Add user validations and encrypted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  before_save :standardize_email
+
   validates :name, presence: true
   validates :email, presence: true
+
+  def standardize_email
+    self.email = email.downcase
+  end
 end
